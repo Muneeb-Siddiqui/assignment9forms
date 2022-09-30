094c7fe0d6b802ea44935f9b24f108ccbfe62a56
Muneeb Siddiqui <pc19427.muneeb@gmail.com>
2022-09-30 17:03:29 +0500

    forms

---
+++ app.js
@@ -0,0 +1,7 @@
+const fname = document.getElementById("colFormLabelSm");
+const lname = document.getElementById("colFormLabel");
+const password = document.getElementById("inputPassword");
+
+if (password.value.lenght > 5){
+    alert("passowrd must be longer them 5 digit")
+}
